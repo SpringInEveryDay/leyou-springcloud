c7aca2c1adef2a16e3d377b689229b21d0ae1f5f
chengzi666 <1125200963@qq.com>
2019-11-28 11:05:24 +0800

    学习springcloud部分的小练习添加02

--- user-consumer-demo/src/main/java/com/leyou/demo/userconsumerdemo/UserConsumerDemoApplication.java
+++ user-consumer-demo/src/main/java/com/leyou/demo/userconsumerdemo/UserConsumerDemoApplication.java
@@ -1,6 +1,7 @@
 package com.leyou.demo.userconsumerdemo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,7 +10,7 @@ import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 //以下在启动类的注解中加入exclude如下内容目的是为了在启动服务的时候不去找配置文件中的内容
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 //在启动类中开启eureka客户端
 @EnableDiscoveryClient
 public class UserConsumerDemoApplication {
